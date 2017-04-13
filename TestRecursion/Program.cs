f2bfcf4c7fdb4f9423666d3bc55dda0ee863c0fc
BrettMerri <the.b.merri@gmail.com>
2017-04-13 12:56:28 -0400

    initial commit

---
+++ TestRecursion/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestRecursion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Fact(5));
+            Console.WriteLine(FactRecursion(5));
+            //FibonacciSeries(50);
+        }
+        public static int Fact(int input)
+        {
+            int Result = 1;
+            for (int i = 1; i <= input; i++)
+            {
+                Result = Result * i;
+            }
+            return Result;
+        }
+        public static int FactRecursion(int input)
+        {
+            if (input == 0)
+                return 1;
+            else
+                return input * FactRecursion(input - 1);
+        }
+        //public static int FibonacciSeries(int input)
+        //{
+        //    if (input == 0)
+        //    {
+        //        return 0;
+        //    }
+        //    if (input == 1)
+        //    {
+        //        return 1;
+        //    }
+        //    else
+        //    {
+        //        Console.WriteLine(input);
+        //        return FactRecursion(input - 1) + FactRecursion(input - 2);
+        //    }
+        }
+    }
+}
